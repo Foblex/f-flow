f3d751a54d095a515250fd3a942b24d3e2d8d7ae
Siarhei Huzarevich <shuzarevich@gmail.com>
2024-08-16 19:46:09 +0200

    feat: Cleanup fDragHandle Directive

--- projects/f-flow/src/f-node/f-drag-handle/f-drag-handle.directive.ts
+++ projects/f-flow/src/f-node/f-drag-handle/f-drag-handle.directive.ts
@@ -1,33 +1,18 @@
 import {
-  Directive, ElementRef, Inject, InjectionToken, Input, OnDestroy, Optional, SkipSelf
+  Directive, ElementRef, InjectionToken
 } from "@angular/core";
-import { Subject } from 'rxjs';
-import { BooleanExtensions, IHasHostElement } from '@foblex/core';
-import { F_NODE } from '../f-node-base';
-import { IHasStateChanges } from '../../i-has-state-changes';
+import { IHasHostElement } from '@foblex/core';
 
 export const F_DRAG_HANDLE: InjectionToken<FDragHandleDirective> = new InjectionToken<FDragHandleDirective>('F_DRAG_HANDLE');
 
 @Directive({
   selector: "[fDragHandle]",
   host: {
-    class: "f-drag-handle f-component",
-    '[class.f-drag-handle-disabled]': 'disabled'
+    class: "f-drag-handle f-component"
   },
   providers: [ { provide: F_DRAG_HANDLE, useExisting: FDragHandleDirective } ],
 })
 export class FDragHandleDirective implements IHasHostElement {
-
-  @Input('fDragHandleDisabled')
-  public get disabled(): boolean {
-    return this.isDisabled;
-  }
-
-  public set disabled(isDisabled: boolean | undefined | string) {
-    this.isDisabled = BooleanExtensions.castToBoolean(isDisabled);
-  }
-
-  private isDisabled: boolean = false;
 
   public get hostElement(): HTMLElement {
     return this.elementReference.nativeElement;
