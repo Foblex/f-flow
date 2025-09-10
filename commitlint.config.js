b47c767ac42768776c685c7cc300367e1e9fe8ad
Siarhei Huzarevich <shuzarevich@gmail.com>
2025-09-10 20:37:51 +0200

    chore: remove husky.sh references from commit-msg and pre-commit scripts

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,19 +6,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-      ],
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'build', 'ci', 'chore', 'revert'],
     ],
   },
 };
