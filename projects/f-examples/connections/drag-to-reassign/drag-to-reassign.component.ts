bc1fb390a39fd368639c61b66e383398584d6031
Siarhei Huzarevich <shuzarevich@gmail.com>
2025-09-14 13:31:38 +0200

    feat: enhance drag-to-reassign component with improved toolbar and connection handling

--- projects/f-examples/connections/drag-to-reassign/drag-to-reassign.component.ts
+++ projects/f-examples/connections/drag-to-reassign/drag-to-reassign.component.ts
@@ -1,10 +1,6 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component, inject, signal, viewChild,
-  ViewChild
-} from '@angular/core';
-import {FCanvasComponent, FFlowModule, FReassignConnectionEvent} from '@foblex/flow';
+import { ChangeDetectionStrategy, Component, signal, viewChild } from '@angular/core';
+import { FCanvasComponent, FFlowModule, FReassignConnectionEvent } from '@foblex/flow';
+import { FCheckboxComponent } from '@foblex/m-render';
 
 @Component({
   selector: 'drag-to-reassign',
@@ -12,37 +8,37 @@ import {FCanvasComponent, FFlowModule, FReassignConnectionEvent} from '@foblex/f
   templateUrl: './drag-to-reassign.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [
-    FFlowModule,
-  ]
+  imports: [FFlowModule, FCheckboxComponent],
 })
 export class DragToReassignComponent {
   private readonly _canvas = viewChild.required(FCanvasComponent);
 
   protected readonly reassignableStart = signal(false);
-  protected readonly connections = signal([{id: '1', source: '1', target: '3'}]);
+  protected readonly connections = signal([{ id: '1', source: '1', target: '3' }]);
 
-  protected onLoaded(): void {
+  protected loaded(): void {
     this._canvas()?.resetScaleAndCenter(false);
   }
 
-  public reassignConnection(event: FReassignConnectionEvent): void {
+  protected reassignConnection(event: FReassignConnectionEvent): void {
     if (!event.newTargetId && !event.newSourceId) {
       return;
     }
     this.connections.update((x) => {
-      const connection = x.find((c) => c.source === event.oldSourceId && c.target === event.oldTargetId);
+      const connection = x.find(
+        (c) => c.source === event.oldSourceId && c.target === event.oldTargetId,
+      );
       if (!connection) {
         throw new Error('Connection not found');
       }
       connection.source = event.newSourceId || connection.source;
       connection.target = event.newTargetId || connection.target;
+
       return [...x];
     });
   }
 
-
-  protected onToggleReassignStart(): void {
+  protected reassignStartChange(): void {
     this.reassignableStart.update((x) => !x);
   }
 }
